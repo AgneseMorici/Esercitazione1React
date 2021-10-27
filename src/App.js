b92112583452b26dc599cab869f2db92122cfaae
Agnese Morici <agni.morici@gmail.com>
2021-10-27 09:01:18 +0200

    ver 1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-
-import { searchMovies } from './api/searchMovies';
 import './App.css';
+import { searchMovies } from './api/searchMovies';
 import {FormRicerca} from './components/FormRicerca/FormRicerca';
 import {Results} from './components/Results/Results';
+import {useState} from "react";
 
 
 
 
 function App() {
-  const results = {
-  
-    const: Results = searchMovies(),
-
+  const [results, setResults] = useState(searchMovies());
+  const handleClick = (type) => {
+    setResults(searchMovies(type));
   }
+   
+
+  
   return (
     <div className="App">
 
+      <button onClick={handleClick("series")}> Cerca Serie </button>
+      <button onClick={handleClick("film")}> Cerca Film </button>
+
       <FormRicerca/>
       <Results pages={results.pages}
-      currentPages={results.currentPages} movie={results.movie} />
+      currentPage={results.currentPage} movie={results.movie} />
        
     </div>
   );
-}
+  }
 
 export default App;
 
-
+  
--- src/api/searchMovies.js
+++ src/api/searchMovies.js
@@ -1,29 +1,67 @@
-export const searchMovies = () =>{
-
+export const searchMovies = (type = "film") =>{
+  if (type === "series") {
     return{
+      movie: [
+        {
+        codice: "a", 
+        title: "film 1",
+        year: 2000,
+        description: "Descrizione film 1",
+        image:"n/a",
+        novità: false
+      },
+
+      {
+        codice: "b", 
+        title: "film 2",
+        year:2000,
+        description: "Descrizione film 1",
+        image:"n/a",
+        novità: false
+      },
+
+      {
+        codice: "a", 
+        title: "film 3",
+        year:2000,
+        description: "Descrizione film 1",
+        image:"n/a",
+        novità: false
+      }
+    ],
+    pages: 10,
+    currentPage: 1
+    }
+  } 
+   else {
+     return{
         movie: [{
-            codice: "a",
-            title: "Titolo movie 1",
+            codice: "a", 
+            title: "film 1",
+            year:2000,
             description: "Descrizione film 1",
             image:"n/a",
             novità: false
           },
           {
             codice: "b",
-            title: "Titolo movie 2",
+            title: "film 2",
+            year:2000,
             description: "Descrizione film 2",
             image:"n/a",
             novità: false
           },
           {
             codice: "c",
-            title: "Titolo movie 3",
+            title: "film 3",
+            year:2000,
             description: "Descrizione film 3",
             image:"n/a",
             novità: false
           }],
-          pages: 10,
+          pages: 20,
           currentPage:1
     }
 
 };
+}
